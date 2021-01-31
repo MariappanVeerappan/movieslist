e065586dab0aaf45c58f01a993e901b33cadea42
Mariappan <mariappanpsg@gmail.com>
2021-01-31 14:40:19 +0530

    Initial commit

---
+++ MoviesListProject/Default.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Title="Movie List" Language="C#" Inherits="MoviesListProject.Default"
+CodeBehind="Default.aspx.cs" AutoEventWireup="true" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Movies Details Page</title>
+            <style>
+        select[id*='ListBox1'] option {
+            white-space:normal;
+        }
+    </style>
+</head>
+<body>
+	<form id="form1" runat="server">
+        <div>
+        <asp:Label ID="Label1" runat="server" Text="Movie Id"></asp:Label>   
+        <asp:TextBox ID="txtFN" runat="server"></asp:TextBox>
+        <asp:Button ID="Button1" runat="server" Text="SUBMIT" onclick="Button1_Click" />
+        </div>
+        <br/>
+        <div style="border: solid 5px blue; float: left; height:400px; width: 200px;">
+        <asp:ListBox ID="ListBox1" runat="server" Text="Movie Names" ItemType="bold" Rows="3" height="400px" width ="200px"
+                SelectionMode="Single"></asp:ListBox>
+        </div>
+        <div style="border: solid 5px red; float: left; height:400px; width: 400px;">    
+                <asp:Image ID="Image1" runat="server" AlternateText ="Movie Poster Path:" height="400px" width ="400px" />
+        </div>       
+
+
+	</form>
+</body>
+</html>
---
+++ MoviesListProject/Default.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+
+
+namespace MoviesListProject
+{
+
+    public partial class Default : System.Web.UI.Page
+    {
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Session["FN"] = txtFN.Text;
+            String movieId = "";
+            if (txtFN.Text == "")
+            {
+                 movieId = "550" + "/"+ "lists";
+            }
+            else
+            {
+                movieId = txtFN.Text;
+            }
+
+            string apiKey = "7837a96568777514c9441f035ef4583a";
+            HttpWebRequest apiRequest = WebRequest.Create("https://api.themoviedb.org/3/movie/" + movieId + "?api_key=" + apiKey + "") as HttpWebRequest;
+
+            string apiResponse = "";
+            using (HttpWebResponse response = apiRequest.GetResponse() as HttpWebResponse)
+            {
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+                apiResponse = reader.ReadToEnd();
+            }
+            ResponseSearchPeople rootObject = JsonConvert.DeserializeObject<ResponseSearchPeople>(apiResponse);
+
+            ListBox1.Items.Clear();
+            ListBox1.Items.Add("Movie Id :  "  + rootObject.id + " ");
+            ListBox1.Items.Add("\n");
+            ListBox1.Items.Add("Movie Name :  " + rootObject.original_title);
+            ListBox1.Items.Add(" \n");
+            ListBox1.Items.Add("OverView :  " + rootObject.overview);
+            ListBox1.Items.Add("\n");
+            Image1.ImageUrl = "https://image.tmdb.org/t/p/w500/" + rootObject.poster_path;
+
+
+        }
+    }
+    public class ResponseSearchPeople
+    {
+        public bool adult { get; set; }
+        public string backdrop_path { get; set; }
+        public List<idname> genres { get; set; }
+        public string original_title { get; set; }
+        public int id { get; set; }
+        public string overview { get; set; }
+        public string poster_path { get; set; }
+
+    }
+
+    public class idname
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+}
